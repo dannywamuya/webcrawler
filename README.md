19c33270d427456eb3bfc190731f43c8bd0ebf4e
dannywamuya <dwamuya@gmail.com>
2021-07-27 09:22:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# *webcrawler*
+Simple web crawler based on jsoup that grabs urls from DOM content and saves to a file
+
+```
+author -      Danny Wamuya  
+date   -      27.07.2021
+lib    -      jsoup-1.14.1.jar 
+```
+
+## *requirements*
+
+Download the <a href="https://jsoup.org/download">jsoup </a> .jar library and include the dependency in your project.
+
+You could also use gradle or maven if you wish.
---
+++ src/Crawler.java
@@ -0,0 +1,43 @@
+/*
+* Simple web crawler based on jsoup that grabs urls from DOM content and saves to a file
+*
+* author -      Danny Wamuya
+* date   -      27.07.2021
+* lib    -      jsoup-1.14.1.jar
+*
+* */
+
+import java.io.FileWriter;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Crawler {
+    public static void main(String[] args) {
+        String url = "https://github.com/";
+        try {
+            getUrls(url);
+        } catch (Exception e) {
+            System.err.println("Error : getUrls\n");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void getUrls(String url) throws Exception {
+
+        FileWriter fWriter = new FileWriter("/Users/Danny/github.txt");
+
+        Connection con = Jsoup.connect(url);
+        Document doc = con.get();
+        Elements ls = doc.select("a[href]");
+
+        for (Element l : ls) {
+            fWriter.write(l.absUrl("href") + "\n");
+            System.out.println(l.absUrl("href"));
+        }
+        fWriter.close();
+    }
+}
